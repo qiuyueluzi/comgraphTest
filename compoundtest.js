5c7586453df60e193d6e67dde8b3c130f514c621
nagasaki <76617420+qiuyueluzi@users.noreply.github.com>
2021-10-22 17:05:47 +0900

    Update compoundtest.js

--- compoundtest.js
+++ compoundtest.js
@@ -1,6 +1,6 @@
 
 
-var elems = { //ノード、エッジを作成
+var elems = {
     nodes: [
         {data: { id: 'A', parent: 'g111'}},
         {data: { id: 'B', parent: 'g112'}},
@@ -32,64 +32,80 @@ var elems = { //ノード、エッジを作成
     ]
 };
 
-var childrenData = new Map(); //サブグラフに含まれるノードを記録する
+
+
 
 var cy = cytoscape({
-    container: document.getElementById('cy'),
-    autounselectify: true,
+  container: document.getElementById('cy'),
+  autounselectify: true,
     style: [
-        {
-            selector: 'node',
-            css: {
-                'content': 'data(id)',
-                'text-halign': 'center',
-                'text-valign': 'center',
-                'background-color': '#20bd3d'
-            }
-        },
+      {
+        selector: 'node',
+        css: {
+          'content': 'data(id)',
+          'text-halign': 'center',
+          'text-valign': 'center',
+          'background-color': '#20bd3d'
+        }
+      },
         
-        {
-            selector: 'node:parent',
-            css: {
+      {
+        selector: 'node:parent',
+        css: {
                 'text-valign': 'top',
                 'text-halign': 'left',
                 'background-opacity': 0.333
-            }
+              }
         },
         
         {
-            selector: 'edge',
-            css: {
-                'line-color': '#09691b',
-                'curve-style': 'bezier',
-                'target-arrow-color': '#09691b',
-                'target-arrow-shape': 'triangle'
-            }
+          selector: 'edge',
+          css: {
+            'content': 'data(id)',
+            'line-color': '#09691b',
+            'curve-style': 'bezier',
+            'target-arrow-color': '#09691b',
+            'target-arrow-shape': 'triangle'
+          }
         }
-    ],
+      ],
     elements: elems,
     layout: {
-        name: 'cola',
-        directed: true,
-        animate: false
+      name: 'cola',
+      directed: true,
+      animate: false
     }
-});
-
-
-var nodes = elems.nodes
-for(var x = 0; x < nodes.length; x++){ //全ノード(サブグラフ)の子ノードの情報を記録
+  });
+  
+  
+  var childrenData = new Map(); //サブグラフに含まれるノードを記録する
+  var edgesData = new Map();
+  var nodes = elems.nodes;
+  var edges = elems.edges;
+  for(var x = 0; x < nodes.length; x++){ //全ノード(サブグラフ)の子ノードの情報を記録
     var curNode = cy.$('#' + nodes[x].data.id);
     var id = curNode.data('id');
-
+    
     var childrenNodes = curNode.children();
     var connectedChildEdges = curNode.children().connectedEdges();
-
+    var connectedEdges = curNode.connectedEdges();
+    var parentNode = nodes[x].data.parent;
     
-    childrenData.set(id, {data:childrenNodes.union(connectedChildEdges), removed: false}); //各ノード(サブグラフ)が持つ、子ノードと子ノードに繋がるエッジを全て記録
-}
+    childrenData.set(id, {node :childrenNodes, childEdge: connectedChildEdges, edge: connectedEdges, parent: parentNode, removed: false});
+  }
+  console.log(childrenData)
+  for(var x = 0; x < edges.length; x++){
+    var curEdge = cy.$('#' + edges[x].data.id);
+    var id = curEdge.data('id');
+    var curSource = curEdge.source();
+    var curTarget = curEdge.target();
 
+    edgesData.set(edges[x].data.id, {source: edges[x].data.source, target: edges[x].data.target, replaced: false});
+    
+  }
+  console.log(edgesData)
 
-var doubleClickDelayMs= 350; //ダブルクリック用
+var doubleClickDelayMs= 350;
 var previousTapStamp;
 cy.on('tap', function(e) {
     var currentTapStamp= e.timeStamp;
@@ -103,8 +119,44 @@ cy.on('doubleTap', 'node', function(){ //フラグに応じて削除・復元
     var nodes = this;
     var id = nodes.data('id')
 
-    if(childrenData.get(id).removed == true){ //サブグラフの削除フラグが立っている場合は復元する
-        childrenData.get(id).data.restore();
+    if(childrenData.get(id).removed == true){
+      childrenData.get(id).node.restore();
+      try{
+        childrenData.get(id).edge.restore();
+        childrenData.get(id).childEdge.restore();
+
+        
+
+        
+      }catch(error){
+        console.log(error.message)
+        var errlog = error.message.split('`');
+        var originID = errlog[1];
+        if(errlog[3]){
+          var nodeID = errlog[3];
+          var parentNode = childrenData.get(nodeID).parent;
+          if(error.message.match('target')){
+            cy.add([{group: 'edges', data:{id: originID, source: edgesData.get(originID).source, target: parentNode}}])
+            //edgesData.set('#' + originID, {source: edgesData.get(originID).source, target: parentNode});
+          }
+          else if(error.message.match('source')){
+            cy.add([{group: 'edges', data:{id: originID, source: parentNode, target: edgesData.get(originID).target}}])
+            //edgesData.set('#' + originID, {source: parentNode, target: edgesData.get(originID).target});
+          }
+          else{
+            console.log('なんかおかしい');
+          }
+          edgesData.get(originID).replaced = true;
+        }
+        else if(edgesData.get(originID).replaced){
+          cy.$('#' + elems.edges[0].data.id).remove();
+          childrenData.get(id).edge.restore();
+          childrenData.get(id).childEdge.restore();
+
+
+        }
+        
+      }
         childrenData.get(id).removed = false;
     } else{
         recursivelyRemove(id, nodes);
@@ -116,19 +168,22 @@ cy.on('doubleTap', 'node', function(){ //フラグに応じて削除・復元
 function recursivelyRemove(id,nodes){ //削除用
     var toRemove = [];
 
+
     for(;;){
-      nodes.forEach(function(node){ //削除対象となるサブグラフ全てに削除フラグを付ける
+      nodes.forEach(function(node){
         childrenData.get(node.data('id')).removed = true;
       });
-
-        Array.prototype.push.apply(toRemove, nodes.children()); //削除対象のノードを配列に追加
-        nodes = nodes.children(); //子サブグラフと子ノードを確認し、孫以下も含め全て削除対象にする
-        if( nodes.empty() ){ break; } //全ての子孫の確認を終えればループ終了
-    }
-
-    for( var i = toRemove.length - 1; i >= 0; i-- ){
-        toRemove[i].remove(); //削除対象を全て削除
+      
+      Array.prototype.push.apply(toRemove, nodes.children());
+        nodes = nodes.children();
+        if( nodes.empty() ){ break; }
+      }
+      
+      for( var i = toRemove.length - 1; i >= 0; i-- ){
+        toRemove[i].remove();
+      }
+    
+      
     }
     
-}
-
+    
